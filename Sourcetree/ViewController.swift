40414acf403942efaf1d46b575c69ed78c6489b8
balandzin <balandzin2602@gmail.com>
2023-12-25 15:49:59 +0300

    from main

--- Sourcetree/ViewController.swift
+++ Sourcetree/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func buttonAction(_ sender: Any) {
+    }
     
     // Commit from merged branches
 }
