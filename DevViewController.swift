9b9aab390863f88c33b0223eabf7a2e8e93a6cb5
balandzin <balandzin2602@gmail.com>
2023-12-25 15:47:19 +0300

    from develop

---
+++ DevViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DevViewController.swift
+//  Sourcetree
+//
+//  Created by Антон Баландин on 25.12.23.
+//
+
+import UIKit
+
+class DevViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+// from develop
+}
