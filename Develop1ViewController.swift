2354896a172950b50ddecc773a2c21fe29966171
balandzin <balandzin2602@gmail.com>
2023-12-25 14:44:14 +0300

    second commit from develop1

--- Develop1ViewController.swift
+++ Develop1ViewController.swift
@@ -16,14 +16,7 @@ class Develop1ViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Commit from develop1
+    
+    // second commit from develop1
 }
